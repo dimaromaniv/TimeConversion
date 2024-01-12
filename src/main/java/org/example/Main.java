baf90e221f3ac2c066f904d72d3f992f832567e5
Dmytro_Romaniv <dimaromaniv@gmail.com>
2024-01-12 16:17:07 -0500

    resolved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,18 +9,25 @@ public class Main {
         String result = "";
         int prefix = 0;
 
-        for (int i = 0 ;i < s.length();  i++){
-            if (s.charAt(8) == 'A') {
+        if (s.charAt(8) == 'A') {
+            if (s.substring(0, 2).equals("12")) {
+                result = "00" + s.substring(2, 8);
+            } else {
+                result = s.substring(0, 8);
+
+            }
+        } else if (s.charAt(8) == 'P') {
+            if (!s.substring(0, 2).equals("12")) {
+                String prefStr = s.substring(0, 2);
+                prefix = Integer.parseInt(prefStr);
+                prefix = prefix + 12;
+                result = prefix + s.substring(2, 8);
+            } else {
                 result = s.substring(0,8);
             }
-            else if (s.charAt(8) == 'P') {
-                String prefNumb = s.substring(0,3);
-                String numericPart = prefNumb.replaceAll("[^0-9]", "");
-                prefix = Integer.parseInt(numericPart);
-                prefix = prefix + 12;
-                result = "" + prefix + s.substring(2,8);
-            }
+
         }
+
         return result;
     }
 
